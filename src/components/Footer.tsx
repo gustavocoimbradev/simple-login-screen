2d02e4c1e302c35d1809010f90785fe63cd9a658
Gustavo <whoisgustavolima@gmail.com>
2025-02-11 16:45:40 -0300

    login-screen

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <div className="bg-white py-10 w-full">
+        <div className="bg-white pt-10 pb-20 xl:py-10 w-full">
             <div className="flex flex-col xl:flex-row items-center max-w-[1118px] mx-auto gap-10 xl:gap-24">
                 <a href="#" className="p-2 py-4 bg-accent text-white flex-1 text-center underline text-sm transition-all ease-in-out duration-300 hover:bg-accent/90"> 
                 Create new account
